002b1cc45e545a8b9ad20eb2a74d37e43eb6ea13
Esraa Khaled <Ekhaled@ntgclarity.com>
2022-08-27 16:36:46 +0200

    Api-Gateway

---
+++ apigw/src/main/java/com/amigoscode/apigw/ApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+}
